c474f0e279a2787dd461d85e3a93ec7c92252810
brahmiyoussef <youssefbrahmi03@gmail.com>
2024-08-29 09:33:54 +0100

    created the controller to see the results of the parsing

--- src/main/java/com/backend/controllers/MTparseController.java
+++ src/main/java/com/backend/controllers/MTparseController.java
@@ -1,2 +1,23 @@
-package com.backend.controllers;public class MTparseController {
+package com.backend.controllers;
+
+import com.backend.domain.entities.mt.message.Message;
+import com.backend.domain.services.mtparsing.parser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MTparseController {
+
+    @Autowired
+    private parser mtMessageService;
+
+    @GetMapping("/parsemt")
+    public Message parseMtMessage(@RequestParam String mtMessage) {
+        // Call the MtMessageService to parse the message
+        Message message = mtMessageService.parseMtMessage(mtMessage);
+        // Return the resulting Message object
+        return message;
+    }
 }
